5db9100814c1f3d2dc7923d66662e939ce1b85d3
Andrewlv <2289380832@qq.com>
2024-04-09 18:22:23 +0800

    feat：gateway实现缓存读取权限

--- code-club-gateway/src/main/java/com/codeclub/gateway/auth/StpInterfaceImpl.java
+++ code-club-gateway/src/main/java/com/codeclub/gateway/auth/StpInterfaceImpl.java
@@ -1,34 +1,49 @@
 package com.codeclub.gateway.auth;
 
 import cn.dev33.satoken.stp.StpInterface;
+import com.alibaba.cloud.commons.lang.StringUtils;
+import com.codeclub.gateway.redis.RedisUtil;
+import com.google.gson.Gson;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 /**
- * 自定义权限验证接口扩展 
+ * 自定义权限验证接口扩展
  */
 @Component
 public class StpInterfaceImpl implements StpInterface {
 
+    @Resource
+    private RedisUtil redisUtil;
+
+    private String authPermissionPrefix = "auth.permission";
+
+    private String authRolePrefix = "auth.role";
+
     @Override
     public List<String> getPermissionList(Object loginId, String loginType) {
-        // 返回此 loginId 拥有的权限列表 
-        // 1. 直接与数据库交互
-        // 2. redis缓存
-        // 3. redis, 没有的话调用微服务获取
-        List<String> permissionList = new LinkedList<>();
-        permissionList.add("user:add");
-        return permissionList;
+        // 返回此 loginId 拥有的权限列表
+        return getAuth(loginId.toString(), authPermissionPrefix);
     }
 
     @Override
     public List<String> getRoleList(Object loginId, String loginType) {
         // 返回此 loginId 拥有的角色列表
-        List<String> roleList = new LinkedList<>();
-        roleList.add("admin");
-        return roleList;
+        return getAuth(loginId.toString(), authRolePrefix);
+    }
+
+    private List<String> getAuth(String loginId, String prefix) {
+        String authKey = redisUtil.buildKey(prefix, loginId);
+        String authValue = redisUtil.get(authKey);
+        if (StringUtils.isBlank(authValue)) {
+            return Collections.emptyList();
+        }
+        List authList = new Gson().fromJson(authValue, List.class);
+        return authList;
     }
 
 }
